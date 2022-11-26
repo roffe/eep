1c2e625542ff6432495e5272baedcbda05a9304f
Roffe <joakim@roffe.nu>
2022-11-27 00:28:52 +0100

    add github structure for releases

---
+++ update/update.go
@@ -0,0 +1,106 @@
+package update
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
+)
+
+type Latest struct {
+	URL             string    `json:"url"`
+	AssetsURL       string    `json:"assets_url"`
+	UploadURL       string    `json:"upload_url"`
+	HTMLURL         string    `json:"html_url"`
+	ID              int       `json:"id"`
+	Author          Author    `json:"author"`
+	NodeID          string    `json:"node_id"`
+	TagName         string    `json:"tag_name"`
+	TargetCommitish string    `json:"target_commitish"`
+	Name            string    `json:"name"`
+	Draft           bool      `json:"draft"`
+	Prerelease      bool      `json:"prerelease"`
+	CreatedAt       time.Time `json:"created_at"`
+	PublishedAt     time.Time `json:"published_at"`
+	Assets          []Assets  `json:"assets"`
+	TarballURL      string    `json:"tarball_url"`
+	ZipballURL      string    `json:"zipball_url"`
+	Body            string    `json:"body"`
+}
+type Author struct {
+	Login             string `json:"login"`
+	ID                int    `json:"id"`
+	NodeID            string `json:"node_id"`
+	AvatarURL         string `json:"avatar_url"`
+	GravatarID        string `json:"gravatar_id"`
+	URL               string `json:"url"`
+	HTMLURL           string `json:"html_url"`
+	FollowersURL      string `json:"followers_url"`
+	FollowingURL      string `json:"following_url"`
+	GistsURL          string `json:"gists_url"`
+	StarredURL        string `json:"starred_url"`
+	SubscriptionsURL  string `json:"subscriptions_url"`
+	OrganizationsURL  string `json:"organizations_url"`
+	ReposURL          string `json:"repos_url"`
+	EventsURL         string `json:"events_url"`
+	ReceivedEventsURL string `json:"received_events_url"`
+	Type              string `json:"type"`
+	SiteAdmin         bool   `json:"site_admin"`
+}
+type Uploader struct {
+	Login             string `json:"login"`
+	ID                int    `json:"id"`
+	NodeID            string `json:"node_id"`
+	AvatarURL         string `json:"avatar_url"`
+	GravatarID        string `json:"gravatar_id"`
+	URL               string `json:"url"`
+	HTMLURL           string `json:"html_url"`
+	FollowersURL      string `json:"followers_url"`
+	FollowingURL      string `json:"following_url"`
+	GistsURL          string `json:"gists_url"`
+	StarredURL        string `json:"starred_url"`
+	SubscriptionsURL  string `json:"subscriptions_url"`
+	OrganizationsURL  string `json:"organizations_url"`
+	ReposURL          string `json:"repos_url"`
+	EventsURL         string `json:"events_url"`
+	ReceivedEventsURL string `json:"received_events_url"`
+	Type              string `json:"type"`
+	SiteAdmin         bool   `json:"site_admin"`
+}
+type Assets struct {
+	URL                string      `json:"url"`
+	ID                 int         `json:"id"`
+	NodeID             string      `json:"node_id"`
+	Name               string      `json:"name"`
+	Label              interface{} `json:"label"`
+	Uploader           Uploader    `json:"uploader"`
+	ContentType        string      `json:"content_type"`
+	State              string      `json:"state"`
+	Size               int         `json:"size"`
+	DownloadCount      int         `json:"download_count"`
+	CreatedAt          time.Time   `json:"created_at"`
+	UpdatedAt          time.Time   `json:"updated_at"`
+	BrowserDownloadURL string      `json:"browser_download_url"`
+}
+
+func GetLatest() (*Latest, error) {
+	latest := new(Latest)
+
+	resp, err := http.Get("https://api.github.com/repos/Hirschmann-Koxha-GbR/eep/releases/latest")
+	if resp != nil && resp.Body != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(b, &latest); err != nil {
+		return nil, err
+	}
+	return latest, nil
+}
