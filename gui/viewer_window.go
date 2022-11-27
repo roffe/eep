5a6e8760ce1f932205481c1836d83ee57776bfa1
Roffe <joakim@roffe.nu>
2022-11-27 04:04:45 +0100

    start XORed add colored sections to viewer

--- gui/viewer_window.go
+++ gui/viewer_window.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"image/color"
 	"io"
+	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
@@ -21,6 +22,7 @@ type viewerWindow struct {
 	toolbar *widget.Toolbar
 	grid    *widget.TextGrid
 	data    []byte
+	cimBin  *cim.Bin
 	saved   bool
 	xor     bool
 }
@@ -32,6 +34,7 @@ func newViewerWindow(e *EEPGui, filename string, data []byte, askSaveOnClose boo
 		w:    w,
 		data: data,
 		grid: widget.NewTextGrid(),
+		xor:  true,
 	}
 	vw.toolbar = vw.newToolbar()
 
@@ -51,6 +54,10 @@ func newViewerWindow(e *EEPGui, filename string, data []byte, askSaveOnClose boo
 
 	for i, row := range generateGrid(vw.data) {
 		vw.grid.SetRow(i, row)
+	}
+	bin, err := cim.MustLoadBytes(filename, data)
+	if err == nil {
+		vw.cimBin = bin
 	}
 
 	w.SetContent(vw.layout())
@@ -105,17 +112,216 @@ func (vw *viewerWindow) newToolbar() *widget.Toolbar {
 }
 
 func (vw *viewerWindow) layout() fyne.CanvasObject {
+	var containers []fyne.CanvasObject
+	if vw.cimBin != nil {
+		infoItems := []string{
+			fmt.Sprintf("MD5: %s", vw.cimBin.MD5()),
+			fmt.Sprintf("CRC32: %s", vw.cimBin.CRC32()),
+			fmt.Sprintf("VIN: %s", vw.cimBin.Vin.Data),
+			fmt.Sprintf("MY: %s", vw.cimBin.ModelYear()),
+			fmt.Sprintf("End model (HW+SW): %d%s", vw.cimBin.PartNo1, vw.cimBin.PartNo1Rev),
+			fmt.Sprintf("Base model (HW+boot): %d%s", vw.cimBin.PnBase1, vw.cimBin.PnBase1Rev),
+			fmt.Sprintf("Delphi part number: %d", vw.cimBin.DelphiPN),
+			fmt.Sprintf("SAAB part number: %d", vw.cimBin.PartNo),
+			fmt.Sprintf("Configuration Version: %d", vw.cimBin.ConfigurationVersion),
+		}
+		/*
+			list := widget.NewList(func() int {
+				return len(infoItems)
+			}, func() fyne.CanvasObject {
+				w := widget.NewLabel("")
+				w.Wrapping = fyne.TextWrapOff
+				w.TextStyle.Monospace = true
+				return w
+			}, func(item widget.ListItemID, obj fyne.CanvasObject) {
+				obj.(*widget.Label).SetText(infoItems[item])
+			})*/
+
+		tg := widget.NewTextGridFromString(strings.Join(infoItems, "\n"))
+		containers = append(containers, tg)
+	}
+
+	containers = append(containers, vw.grid)
+
 	return container.NewBorder(vw.toolbar, nil, nil, nil,
-		container.New(layout.NewMaxLayout(),
-			vw.grid,
+		container.New(layout.NewHBoxLayout(),
+			containers...,
 		),
 	)
+}
+
+type colorDesc struct {
+	name  string
+	start int
+	end   int
+	color color.RGBA
+}
+
+var (
+	colorChecksum = rgb(0, 255, 30)
+	colorUnknown  = rgb(33, 33, 33)
+
+	colorList = []colorDesc{
+		{
+			name:  "Programming date",
+			start: 0x0,
+			end:   0x3,
+			color: rgb(128, 128, 0),
+		},
+
+		{
+			name:  "Sas Option",
+			start: 0x4,
+			end:   0x4,
+			color: rgb(50, 200, 0),
+		},
+		{
+			name:  "Unknown Bytes 1",
+			start: 0x5,
+			end:   0xa,
+			color: colorUnknown,
+		},
+		{
+			name:  "PartNo 1",
+			start: 0xb,
+			end:   0xe,
+			color: rgb(160, 18, 34),
+		},
+		{
+			name:  "PartNo 1 Revision",
+			start: 0xf,
+			end:   0x10,
+			color: rgb(60, 60, 10),
+		},
+		{
+			name:  "Configuration Version",
+			start: 0x11,
+			end:   0x14,
+			color: rgb(255, 0, 255),
+		},
+		{
+			name:  "PNBase",
+			start: 0x15,
+			end:   0x18,
+			color: rgb(45, 72, 200),
+		},
+		{
+			name:  "PNBase Revision",
+			start: 0x19,
+			end:   0x1a,
+			color: rgb(100, 100, 43),
+		},
+		{
+			name:  "VIN Data",
+			start: 0x1b,
+			end:   0x2b,
+			color: rgb(200, 30, 76),
+		},
+		{
+			name:  "VIN Value",
+			start: 0x2c,
+			end:   0x2c,
+			color: rgb(240, 240, 10),
+		},
+		{
+			name:  "VIN Unknown",
+			start: 0x2d,
+			end:   0x35,
+			color: rgb(35, 156, 63),
+		},
+		{
+			name:  "VIN SPS Count",
+			start: 0x36,
+			end:   0x36,
+			color: rgb(66, 22, 88),
+		},
+		{
+			name:  "VIN Checksum",
+			start: 0x37,
+			end:   0x38,
+			color: colorChecksum,
+		},
+		{
+			name:  "Programming ID",
+			start: 0x39,
+			end:   0x56,
+			color: rgb(72, 140, 38),
+		},
+		{
+			name:  "Unknown Data 3 Bank #1",
+			start: 0x57,
+			end:   0x80,
+			color: colorUnknown,
+		},
+		{
+			name:  "Unknown Data 3 Bank #1 CRC",
+			start: 81,
+			end:   0x82,
+			color: colorChecksum,
+		},
+		{
+			name:  "Unknown Data 3 Bank #2",
+			start: 0x83,
+			end:   0xac,
+			color: colorUnknown,
+		},
+		{
+			name:  "Unknown Data 3 Bank #2 CRC",
+			start: 0xad,
+			end:   0xae,
+			color: colorChecksum,
+		},
+		{
+			name:  "PIN Data",
+			start: 0xaf,
+			end:   0xb2,
+			color: rgb(56, 89, 217),
+		},
+		{
+			name:  "PIN Unknown",
+			start: 0xb3,
+			end:   0xb6,
+			color: rgb(56, 89, 217),
+		},
+		{
+			name:  "PIN CRC",
+			start: 0xb7,
+			end:   0xb8,
+			color: colorChecksum,
+		},
+		/*
+			{
+				name:  "",
+				start: 0x,
+				end:   0x,
+				color: rgb(),
+			},
+		*/
+	}
+)
+
+func rgb(r, g, b uint8) color.RGBA {
+	return color.RGBA{R: r, G: g, B: b, A: 1}
+}
+
+func viewColor(pos int) color.RGBA {
+	for _, c := range colorList {
+		if (c.start == c.end) && c.start == pos {
+			return c.color
+		}
+		if pos >= c.start && pos <= c.end {
+			return c.color
+		}
+	}
+	return rgb(255, 255, 255)
 }
 
 func generateGrid(data []byte) []widget.TextGridRow {
 	var rows []widget.TextGridRow
 	r := bytes.NewReader(data)
 	buff := make([]byte, 32)
+	pos := 0
+	rPos := 0
 	for {
 		n, err := r.Read(buff)
 		if err != nil {
@@ -129,17 +335,14 @@ func generateGrid(data []byte) []widget.TextGridRow {
 			row.Cells = append(row.Cells, widget.TextGridCell{
 				Rune: rune(asd[0]),
 				Style: &widget.CustomTextGridStyle{
-					FGColor: color.RGBA{
-						R: 60, G: 128, B: 0, A: 1,
-					},
+					FGColor: viewColor(pos),
 				},
 			})
+
 			row.Cells = append(row.Cells, widget.TextGridCell{
 				Rune: rune(asd[1]),
 				Style: &widget.CustomTextGridStyle{
-					FGColor: color.RGBA{
-						R: 60, G: 128, B: 0, A: 1,
-					},
+					FGColor: viewColor(pos),
 				},
 			})
 			if x < 31 {
@@ -147,7 +350,25 @@ func generateGrid(data []byte) []widget.TextGridRow {
 					Style: widget.TextGridStyleWhitespace,
 				})
 			}
+			pos++
 		}
+
+		if n < 32 {
+			for ex := n; ex < 32; ex++ {
+				row.Cells = append(row.Cells, widget.TextGridCell{
+					Style: widget.TextGridStyleWhitespace,
+				})
+				row.Cells = append(row.Cells, widget.TextGridCell{
+					Style: widget.TextGridStyleWhitespace,
+				})
+				if ex < 31 {
+					row.Cells = append(row.Cells, widget.TextGridCell{
+						Style: widget.TextGridStyleWhitespace,
+					})
+				}
+			}
+		}
+
 		row.Cells = append(row.Cells, widget.TextGridCell{
 			Rune:  rune('║'),
 			Style: widget.TextGridStyleWhitespace,
@@ -157,13 +378,11 @@ func generateGrid(data []byte) []widget.TextGridRow {
 			row.Cells = append(row.Cells, widget.TextGridCell{
 				Rune: rune(bb),
 				Style: &widget.CustomTextGridStyle{
-					FGColor: color.RGBA{
-						R: 60, G: 128, B: 128, A: 1,
-					},
+					FGColor: viewColor(rPos),
 				},
 			})
+			rPos++
 		}
-
 		rows = append(rows, row)
 	}
 	return rows
